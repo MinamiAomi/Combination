10561bb12e5c31ea027e1f0c51d55fe26f58119e
MinamiAomi <k022g0040@g.neec.ac.jp>
2023-01-19 11:26:54 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -57,21 +57,30 @@ constexpr int combination(int n, int r) {
 
 using namespace std;
 
+struct Combi {
+	int indexX; // 組み合わせの右側
+	int indexY; // 組み合わせの左側
+	int x; // 左辺
+	int y; // 右辺
+	int z; // 結果
+};
+
+std::ostream& operator<<(std::ostream& o, const Combi& c) {
+	return o << Format("[%2d:%2d] %d * %d = %2d", c.indexX,c.indexY,c.x,c.y,c.z) <<  std::endl;
+}
+
 int main() {
 
 	srand((unsigned int)time(nullptr));
 	
 	const int kArrayConst = 10;
 	const int kPair = 2;
-	const int comb = combination(kArrayConst, kPair);
 	int array[kArrayConst] = {};
 	
 	{
 		int i = 0;
 		for (auto& it : array) {
-			//it = rand() % 10;
-			it = i;
-			i++;
+			it = rand() % 10;			
 		}
 	}
 	
@@ -82,34 +91,33 @@ int main() {
 			std::cout << ", ";
 		}
 		else {
-			std::cout << std::endl;
-		}
-	}
-
-	std::cout << std::endl << Format("%d C %d = %d", kArrayConst, kPair, comb) << std::endl << std::endl;
-	
-	std::array<std::array<int, kPair>, comb> index;
-	{
-		int i, j;
-		int arrange[2] = {};
-
-		
-		for (int k = 0; k < comb; k++) {
-			for (i = kPair - 1; 0 <= i; i--) {
-				if (arrange[i] < kArrayConst - kPair + i) {
-					arrange[i] += 1;
-					for (j = i; j < kPair - 1; j++) {
-						arrange[j + 1] = arrange[j] + 1;
-					};
-					break;
-				}
-			}
-			for (i = 0; i < kPair; i++) {
-				index.at(k).at(i) = array[arrange[i]];
-			}
+			std::cout << std::endl << std::endl;
 		}
 	}
 	
+	// arrayの組み合わせをリストアップ
+	// 組み合わせが入るリスト
+	std::vector<Combi> combiList;
+	// 左側ループ 0 ~ i ~ n - 1
+	for (int i = 0; i < kArrayConst - 1; i++) {
+		// 右側ループ i ~ j ~ n
+		for (int j = i + 1; j < kArrayConst; j++) {
+			Combi tmp = {};
+			tmp.indexX = i;
+			tmp.indexY = j;
+			tmp.x = array[i];
+			tmp.y = array[j];
+			tmp.z = tmp.x * tmp.y;
+			// リストの最後尾に追加
+			combiList.emplace_back(tmp);
+		}
+	}
+	// リストを出力
+	for (const auto& it : combiList) {
+		std::cout << it;
+	}
+	
+	std::cout << std::endl << Format("%d C %d = %d", kArrayConst, kPair, combiList.size());
 
 	End();
 	return 0;
